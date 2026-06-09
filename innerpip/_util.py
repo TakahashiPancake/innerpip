f5ddf01148c170be16cf614874e1301a83dd556a
TakahashiPancake <LuxAEterna@FoxMail.com>
2026-06-09 18:39:46 +0800

    feat: 新增可扩展字符IO

--- innerpip/_util.py
+++ innerpip/_util.py
@@ -5,7 +5,10 @@ Licensed under MIT
 
 """
 import sys
+import io
+from typing import TextIO
 from typing import Callable, Any
+
 
 def del_module_by_name(module_name: str) -> Callable:
   """
@@ -27,4 +30,36 @@ def del_module_by_name(module_name: str) -> Callable:
       return result
     return wrapper
   return decorator
+
+
+class ExtendedStringIO(io.StringIO):
+
+  # 定义多输出
+  __outputs: list[TextIO]
+
+  def __init__(self, *sync: TextIO) -> None:
+    """
+    Args:
+      sync:   同步输出流
+
+    Returns:
+      return: 无
+
+    """
+    # 同步输出到流
+    self.__outputs = list(sync)
+
+    # 调用父类构造方法
+    super().__init__()
+
+
+  # 重写write()方法
+  def write(self, s: str, /) -> int:
+
+    # 将字符串打印到多输出
+    for o in self.__outputs:
+      o.write(s)
+
+    # 调用父类write()方法
+    return super().write(s)
 
